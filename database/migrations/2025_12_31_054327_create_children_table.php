ea5d7fd886998657af7190d4dac705c9dc019068
rchmdndy <rachmadandyms@gmail.com>
2025-12-31 12:59:57 +0700

    feat : create children migration

--- database/migrations/2025_12_31_054327_create_children_table.php
+++ database/migrations/2025_12_31_054327_create_children_table.php
@@ -7,12 +7,29 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
+
+
+
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('children', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")
+                    ->comment("foreign to users table with the role 'parent'")
+                    ->constrained("users", "id", "fk_users_idx")
+                    ->nullOnDelete()->cascadeOnUpdate();
+            $table->string("name");
+            $table->date("birthday");
+            $table->enum("gender", ["male", "female", "other"]);
+            $table->string("avatar_url")->nullable();
+            $table->decimal("birth_weight", 5, 2);
+            $table->decimal("birth_height", 5, 2);
+            $table->decimal("head_circumference", 5, 2);
+            $table->boolean("is_active")->default(true);
+            $table->text("note")->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
